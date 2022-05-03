9e402cddf58cd73d637fc02e4f16ff46024cd282
Bepsommer <gtgiorgiot@gmail.com>
2022-05-03 15:19:47 +0200

    Creazione package Control

---
+++ Entity/Annucio.php
@@ -0,0 +1,111 @@
+<?php
+
+class Annucio
+{
+    private string $titolo;
+    private string $descrizione;
+    private float $prezzo;
+    private Foto $foto;
+    private Date $data;
+
+    /**
+     * @param string $titolo
+     * @param string $descrizione
+     * @param float $prezzo
+     * @param Foto $foto
+     * @param Date $data
+     */
+    public function __construct(string $titolo, string $descrizione, float $prezzo, Foto $foto, Date $data)
+    {
+        $this->titolo = $titolo;
+        $this->descrizione = $descrizione;
+        $this->prezzo = $prezzo;
+        $this->foto = $foto;
+        $this->data = $data;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescrizione(): string
+    {
+        return $this->descrizione;
+    }
+
+    /**
+     * @param string $descrizione
+     */
+    public function setDescrizione(string $descrizione): void
+    {
+        $this->descrizione = $descrizione;
+    }
+
+    /**
+     * @return float
+     */
+    public function getPrezzo(): float
+    {
+        return $this->prezzo;
+    }
+
+    /**
+     * @param float $prezzo
+     */
+    public function setPrezzo(float $prezzo): void
+    {
+        $this->prezzo = $prezzo;
+    }
+
+    /**
+     * @return Foto
+     */
+    public function getFoto(): Foto
+    {
+        return $this->foto;
+    }
+
+    /**
+     * @param Foto $foto
+     */
+    public function setFoto(Foto $foto): void
+    {
+        $this->foto = $foto;
+    }
+
+    /**
+     * @return Date
+     */
+    public function getData(): Date
+    {
+        return $this->data;
+    }
+
+    /**
+     * @param Date $data
+     */
+    public function setData(Date $data): void
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitolo(): string
+    {
+        return $this->titolo;
+    }
+
+    /**
+     * @param string $titolo
+     */
+    public function setTitolo(string $titolo): void
+    {
+        $this->titolo = $titolo;
+    }
+
+
+
+
+
+}
