79db78ee19183fbedca249cc86218ede29224e82
Bepsommer <gtgiorgiot@gmail.com>
2022-05-29 17:10:55 +0200

    Creazione di FRecensione.php,con metodi load, mancano metodi admin

--- Foundation/FRecensione.php
+++ Foundation/FRecensione.php
@@ -15,10 +15,50 @@ public static function bind($stmt,ERecensione $recensione){
         $stmt->bindValue(':idRecensione',$recensione->getIdRecensione(),PDO::PARAM_INT);
         $stmt->bindValur(':idUser',$recensione->getIdUser());
 }
-public function load(){
-
+// metodo che crea un oggetto da una riga della tabella recensione
+public function getFromRow($row)
+{
+        $rece = new ERecensione($row['testo'],$row['data'],$row['idProdotto'],$row['idRecensione'],$row['idUser']);
+        $rece->setIdRecensione($row['idRecensione']);
+        return $rece; // ci vorrebbe anche un setBanato per le recensioni
 }
-public function contaRece(){
+public function loadById($id){
+        $row = parent::loadbyId($id);//attraverso il metodo della classe padre restituisco la riga
+        $arrece = $row[0];
+        if(($row!=null) && (count($row)>0)){
+            $rece = $this->getFromRow($arrece);
+            return $rece;
+    }
+        else return null;{
+    }
+}
+// metodo load di recensioni dal db dato un gruppo di id
+public function loadByAllIds($ids){
+        $array = parent:: loadbyallIds($ids);
+        $arrayObj = array();
+        if (($array=!null)&&(count($array)>0)){
+            foreach ($array as $i){
+                $rece = $this->getFromRow($i);
+                array_push($arrayObj,$rece);
+            }
+            return $arrayObj;
+        }
+        else return null;
+}
+public function search($attributo, $valore){
+    $array = parent::search($attributo, $valore);
+    $arrayobj = array();
+    if(($array!=null)&&(count($array)>0)){
+        foreach ($array as $i){
+            $rece = $this->getFromRow($i);
+            array_push($arrayobj,$rece);
+        }
+        return $arrayobj;
+    }
+    else return null;
+}
+
+    public function contaRece(){
     $query ="SELECT COUNT(idRecensione) AS n FROM ".$this->table.";";
     try{
         $this->localmp->beginTransaction();
