d5d742211e20b79ad27b16074101ee031466a037
Bepsommer <gtgiorgiot@gmail.com>
2022-07-07 18:29:23 +0200

    creazione FUtente.php

--- Foundation/FUtente.php
+++ Foundation/FUtente.php
@@ -1,7 +1,16 @@
 <?php
-/* La classe FUtente fornisce query per gli oggetti EUtente */
+/** La classe FUtente fornisce query per gli oggetti EUtente
+ * @author Gruppo 7
+ * @package Foundation
+ */
 class FUtente extends FDatabase
 {
+    /** @var string tabella con cui opera */
+    private static $table = "utente";
+    /** classe foundation  */
+    private static $class = "FUtente";
+    /** valori della tabella */
+    private static $values = '(:nome, :cognome, :username, :password, :email, :annunci, :recensioni, :storico, :idUser)';
     public function __construct()
     {
         parent::__construct(); //richiama il costruttore di FDatabase
@@ -10,7 +19,10 @@ class FUtente extends FDatabase
         $this->class = 'FUtente';
     }
 
-// metodo che lega gli attributi dell'utente che si vogliono inserire con i parametri della INSERT
+/** Questo metodo lega gli attributi dell'Utente da inserire con i parametri della INSERT
+ *@param PDOStatement $stmt
+ *@param EUtente $user Utente in cui i dati devono essere inseriti nel DB
+ */
     public static function bind($stmt, EUtente $user)
     {
         $stmt->bindValue(':idUser', NULL, PDO::PARAM_INT);
@@ -20,6 +32,117 @@ class FUtente extends FDatabase
         $stmt->bindValue(':password', $user->getPassword(), PDO::PARAM_STR);
         $stmt->bindValue(':email', $user->getEmail(), PDO::PARAM_STR);
     }
+    /** Questo metodo restituisce il nome della tabella per la costruzione della Query
+     *@return string $table nome della tabella
+     */
+    public static function getTable(): string {
+        return self::$table;
+    }
+    /** Questo metodo restituisce il nome della classe per la costruzione della Query
+     *@return string $class nome della classe
+     */
+    public static function getClass(): string {
+        return self::$table;
+    }
+    /** Questo metodo restituisce l'insieme dei valori per la costruzione della Query */
+    public static function getValues() :string {
+        return self::$values;
+    }
+    /** Metodo che permette la store di un utente
+     *@param $utente utente da salvare
+     */
+    public static function store($utente){
+        $db = parent::getInstance();
+        $id = $db->insertDb(self::$class, $utente);
+        $utente->setId($id);
+    }
+    /** Metodo che può aggiornare i campi di un utente
+     * @param $val valore della primary key da usare come riferimento per la riga
+     * @param $newValue nuovo valore da assegnare
+     * @param $field campo in cui si vuole modificare il valore
+     * @param $pk
+     * @return true se esiste il mezzo, altrimenti false
+     */
+    public static function update($field, $newValue, $pk, $val){
+        $db = parent::getInstance();
+        $result = $db->updateDB(self::getClass(),$field,$newValue,$pk,$val);
+        if($result) return true;
+        else return false;
+    }
+    /** Metodo che permette la delete sul db in base all'id
+     *@param $id id dell'oggetto da eliminare dal db
+     */
+    public static function delete($field,$id){
+        $db = parent::getInstance();
+        $result = $db->deleteDB(self::getClass(),$field,$id);
+        if($result != null) return true;
+        else return false;
+    }
+    /** Metodo che permette di verificare se esiste un utente nel DB
+     *@param $id valore della riga di cui si vuole verificare l'esistenza
+     *@param $field colonna su cui eseguire la verifica
+     */
+    public static function exist($field,$id){
+        $db = parent::getInstance();
+        $result = $db->existDB(self::getClass(),$field,$id);
+        if ($result != null) return true;
+        else return false;
+    }
+    /** Metodo che cerca un determinato oggetto nel DB */
+    public static function search($parametri=array(),$ordinamento='',$limite=''){
+        $db = parent::getInstance();
+        $result = $db->searchDB(self::$class,$parametri,$ordinamento,$limite);
+        return $result;
+    }
+    /** Metodo che permette il caricamento del login di un utente, passati la sua email e password
+     *@param email dell'utente
+     * @param pass password dell'utente
+     */
+    public static function loadLogin($user,$pass){
+        $utente = null;
+        $db = FDatabase::getInstance();
+        $result = $db->checkIfLogged($user,$pass);
+        if(isset($result)){
+            $utente = self::loadByField(array(['email', '=', $result['email']]));
+        }
+    }
+    /** Metodo che prende determinate righe dal DB */
+    public static function getRows($parametri = array(),$ordinamento = '',$limite = ''){
+        $db = parent::getInstance();
+        $result = $db->getRowNum(self::$class,$parametri,$ordinamento,$limite);
+        return $result;
+    }
+    /** Metodo che permette la load su db
+     * @return object $utente utente loggato
+     */
+    public static function loadByField($parametri = array(),$ordinamento = '',$limite = ''){
+        $utente = null;
+        $db = parent::getInstance();
+        $result = $db->searchDb(static::getClass(),$parametri,$ordinamento,$limite);
+        if(count($parametri)>0){
+            $rows_number = $db->getRowNum(static::getClass(), $parametri);
+        } else{
+            $rows_number = $db->getRowNum(static::getClass());
+        }
+        if(($result != null) && ($rows_number > 1)){
+            $utente = new EUtente($result['nome'],$result['cognome'],$result['username'],$result['password'],['email'],$result['idUser']); // idFoto
+        }
+        else{
+            if(($result != null) && ($rows_number > 1)){
+                $utente = array();
+                for($i = 0; count($result);$i++){
+                    $utente[] = new EUtente($result['nome'],$result['cognome'],$result['username'],$result['password'],['email'],$result['idUser'];)
+                }
+            }
+        }
+        return $utente;
+    }
+
+
+
+
+
+
 
 // metodo che crea un oggetto EUtente a partire dalla tupla della tabella utente
     public function getFromRow($row)
@@ -158,22 +281,7 @@ class FUtente extends FDatabase
         }
     }
 
-    public  function store($utente){
-        $id=parent::store($utente);
-        if ($id) {
 
-            $foto = file_get_contents('./pics/profile.png');
-            $fotoObj = new EFoto($foto, 'pic/png');
-            $fotoObj->setIdEst($id);
-            $fotoUt = new FFotoUtente();
-            $fotoUt->store($fotoObj);
-            return $id;
-        }
-        else {
-            return false;
-        }
-
-    }
 
     public function utentiByString ($array, $toSearch)
     {
