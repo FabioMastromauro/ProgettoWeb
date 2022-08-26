3287de77a3991e142b5be8001299115d39e2f15c
Civita2107 <f.civitareale@gmail.com>
2022-08-26 19:43:57 +0200

    Finito CFrontController.php, manca un ultimo pezzo da fare insieme

--- Control/CFrontController.php
+++ Control/CFrontController.php
@@ -5,7 +5,7 @@ class CFrontController
 
     public function run ($path) {
         $method = $_SERVER['REQUEST_METHOD'];
-
+        // loro ci hanno aggiunto qualocsa
         $resource = explode("/", $path);
 
         array_shift($resource);
@@ -22,6 +22,26 @@ class CFrontController
                     if (method_exists($controller, $function)) {
 
                         $param = array();
+                    }
+                    for ($i =2; $i < count($resource); $i++) {
+                        $param[] = $resource[$i];
+                        $a = $i - 2; //non so a cosa serva
+                    }
+                    $num = count($param);
+
+                    if ($num == 0) {
+                        $controller::$function();
+                    }
+                    elseif ($num == 1) {
+                        $controller::$function($param[0]);
+                    }
+                    elseif ($num == 2) {
+                        $controller::$function($param[0], $param[1]);
+                    }
+                }
+                else {
+                    if (CUtente::isLogged()) {
+                        //dobbiamo vedere come implementare
                     }
                 }
             }
