61017209e82aef2010202a71c8e5e9fdeb38d67a
Civita2107 <f.civitareale@gmail.com>
2022-08-28 16:56:01 +0200

    Modifiche StartSmarty.php

--- StartSmarty.php
+++ StartSmarty.php
@@ -6,10 +6,10 @@ class StartSmarty
 {
     static function configuration() {
         $smarty = new Smarty();
-        $smarty->template_dir = 'smarty/templates/';
-        $smarty->compile_dir = 'smarty/templates_c/';
-        $smarty->config_dir = 'smarty/configs/';
-        $smarty->cache_dir = 'smarty/cache/';
+        $smarty->template_dir = 'smarty/libs/templates/';
+        $smarty->compile_dir = 'smarty/libs/templates_c/';
+        $smarty->config_dir = 'smarty/libs/configs/';
+        $smarty->cache_dir = 'smarty/libs/cache/';
         return $smarty;
     }
 }
