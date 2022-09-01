49a9142256b9e3f79bcbb08c712ad02a46155766
Civita2107 <f.civitareale@gmail.com>
2022-09-01 18:34:50 +0200

    Aggiunti VAdmin.php e VAnnunci.php

---
+++ View/VAnnunci.php
@@ -0,0 +1,33 @@
+<?php
+
+class VAnnunci
+{
+    private $smarty;
+
+    public function __construct() {
+        $this->smarty = StartSmarty::configuration();
+    }
+
+    static function getTitoloAnnuncio() {
+        return strtoupper($_POST['title']);
+    }
+
+    static function getDescrizioneAnnuncio() {
+        return $_POST['descrizione'];
+    }
+
+    static function getPrezzoAnnuncio() {
+        return $_POST['prezzo'];
+    }
+
+    static function getCategoriaAnnuncio() {
+        return $_POST['categoria'];
+    }
+
+    // static function get
+
+    function showAnnunci($annunci, $array=null) {
+        if (CUtente::isLogged()) $this->smarty->assign('userlogged', 'logged');
+
+    }
+}
