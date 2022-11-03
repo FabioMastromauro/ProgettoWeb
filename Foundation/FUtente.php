188f8889644cd133c237c55913e9a4997f3ca790
Bepsommer <gtgiorgiot@gmail.com>
2022-11-03 13:01:39 +0100

    aggiunto php docBlock FUtente.php

--- Foundation/FUtente.php
+++ Foundation/FUtente.php
@@ -1,23 +1,34 @@
 <?php
-/** La classe FUtente fornisce query per gli oggetti EUtente
+/**
+ * La classe FUtente fornisce query per gli oggetti EUtente
  * @author Gruppo 7
  * @package Foundation
  */
 class FUtente extends FDatabase
 {
-    /** @var string tabella con cui opera */
+    /**
+     * @var string tabella con cui opera
+     */
     private static $table = "utente";
-    /** classe foundation  */
+    /**
+     * Classe foundation
+     */
     private static $class = "FUtente";
-    /** valori della tabella */
+    /**
+     * Valori della tabella
+     */
     private static $values = '(:nome, :cognome, :idUser, :email, :password, :idFoto, :dataIscrizione, :dataFineBan, :ban, :admin)';
+    /**
+     * Costruttore
+     */
     public function __construct(){}
 
 
-/** Questo metodo lega gli attributi dell'Utente da inserire con i parametri della INSERT
- *@param PDOStatement $stmt
- *@param EUtente $user Utente in cui i dati devono essere inseriti nel DB
- */
+    /**
+     * Questo metodo lega gli attributi dell'utente da inserire con i parametri della insert
+     * @param PDOStatement $stmt
+     * @param EUtente $user Utente in cui i dati devono essere inseriti nel DB
+     */
     public static function bind($stmt, EUtente $user)
     {
         $stmt->bindValue(':idUser', $user->getIdUser(), PDO::PARAM_INT);
@@ -31,24 +42,33 @@ class FUtente extends FDatabase
         $stmt->bindValue(':ban', $user->isBan(), PDO::PARAM_BOOL);
         $stmt->bindValue(':admin', $user->getAdmin(), PDO::PARAM_BOOL);
     }
-    /** Questo metodo restituisce il nome della tabella per la costruzione della Query
-     *@return string $table nome della tabella
+
+    /**
+     * Questo metodo restituisce il nome della tabella per la costruzione della query
+     * @return string $table nome della tabella
      */
     public static function getTable(): string {
         return self::$table;
     }
-    /** Questo metodo restituisce il nome della classe per la costruzione della Query
-     *@return string $class nome della classe
+    /**
+     * Questo metodo restituisce il nome della classe per la costruzione della query
+     * @return string $class nome della classe
      */
     public static function getClass(): string {
         return self::$class;
     }
-    /** Questo metodo restituisce l'insieme dei valori per la costruzione della Query */
+
+    /**
+     * Questo metodo restituisce l'insieme dei valori per la costruzione della Query
+     */
     public static function getValues() :string {
         return self::$values;
     }
-    /** Metodo che permette la store di un utente
-     *@param $utente utente da salvare
+
+    /**
+     * Metodo che permette la store di un utente
+     * @param $utente utente da salvare
+     * @return null
      */
     public static function store($utente){
 
@@ -56,7 +76,9 @@ class FUtente extends FDatabase
         $id = $db->storeDB(self::$class, $utente);
         $utente->setIdUser($id);
     }
-    /** Metodo che può aggiornare i campi di un utente
+
+    /**
+     * Metodo che può aggiornare i campi di un utente
      * @param $val valore della primary key da usare come riferimento per la riga
      * @param $newValue nuovo valore da assegnare
      * @param $field campo in cui si vuole modificare il valore
@@ -69,8 +91,12 @@ class FUtente extends FDatabase
         if($result) return true;
         else return false;
     }
-    /** Metodo che permette la delete sul db in base all'id
-     *@param $id id dell'oggetto da eliminare dal db
+
+    /**
+     * Metodo che permette la delete sul DB in base all'id
+     * @param $field campo da considerare quando viene passat l'id
+     * @param $id id dell'oggetto da eliminare dal DB
+     * @return true|false
      */
     public static function delete($field,$id){
         $db = parent::getInstance();
@@ -78,9 +104,12 @@ class FUtente extends FDatabase
         if($result != null) return true;
         else return false;
     }
-    /** Metodo che permette di verificare se esiste un utente nel DB
-     *@param $id valore della riga di cui si vuole verificare l'esistenza
-     *@param $field colonna su cui eseguire la verifica
+
+    /**
+     * Metodo che permette di verificare se esiste un utente nel DB
+     * @param $id valore della riga di cui si vuole verificare l'esistenza
+     * @param $field colonna su cui eseguire la verifica
+     * @return true|false
      */
     public static function exist($field,$id){
         $db = parent::getInstance();
@@ -88,15 +117,25 @@ class FUtente extends FDatabase
         if ($result != null) return true;
         else return false;
     }
-    /** Metodo che cerca un determinato oggetto nel DB */
+
+    /**
+     * Metodo che cerca un determinato oggetto nel DB
+     * @param array $parametri
+     * @param string $ordinamento
+     * @param string $limite
+     * @return mixed
+     */
     public static function search($parametri=array(),$ordinamento='',$limite=''){
         $db = parent::getInstance();
         $result = $db->searchDB(self::$class,$parametri,$ordinamento,$limite);
         return $result;
     }
-    /** Metodo che permette il caricamento del login di un utente, passati la sua email e password
-     *@param email dell'utente
+
+    /**
+     * Metodo che permette il caricamento di un utente, posto che sia loggato
+     * @param email dell'utente
      * @param pass password dell'utente
+     * @return mixed
      */
     public static function loadLogin($user,$pass){
         $utente = null;
@@ -107,21 +146,33 @@ class FUtente extends FDatabase
         }
         return $utente;
     }
-    /** Metodo che prende determinate righe dal DB */
+
+    /**
+     * Metodo che prende determinate righe dal DB
+     * @param array $parametri
+     * @param string $ordinamento
+     * @param string $limite
+     * @return mixed
+     */
     public static function getRows($parametri = array(),$ordinamento = '',$limite = ''){
         $db = parent::getInstance();
         $result = $db->getRowNum(self::$class,$parametri,$ordinamento,$limite);
         return $result;
     }
-    /** Metodo che permette la load su db
-     * @return object $utente utente loggato
+
+    /**
+     * Metodo che permette la load di uno o più utenti da DB
+     * @param array $parametri
+     * @param string $ordinamento
+     * @param string $limite
+     * @return object $utente -> utenti risultanti dalla query
      */
     public static function loadByField($parametri = array(), $ordinamento = '', $limite = ''){
         $utente = null;
         $db = parent::getInstance();
         $result = $db->searchDB(static::getClass(),$parametri, $ordinamento,$limite);
         if(count($parametri)>0){
-             $rows_number = $db->getRowNum(static::getClass(), $parametri,$ordinamento,$limite);
+            $rows_number = $db->getRowNum(static::getClass(), $parametri,$ordinamento,$limite);
         } else{
             $rows_number = $db->getRowNum(static::getClass());
         }
