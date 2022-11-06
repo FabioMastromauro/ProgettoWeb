767e460864a5adbf02d14c413785a3cca6fb7170
Civita2107 <f.civitareale@gmail.com>
2022-11-06 17:38:25 +0100

    Ultra correzione CRicerca.php

--- Control/CRicerca.php
+++ Control/CRicerca.php
@@ -16,18 +16,16 @@ class CRicerca
         $check = 0;
 
         for ($i = 0; $i < 2; $i++){
-
-                while($check!=1) {
-
-
-            $new_num = rand(0, $numAnnunci - 1);
-             if (!in_array($new_num, $ran_num)) {
-            $ran_num[] = $new_num;
-            $check = 1;
-                  }
-
-                    }
             $annunci_id = $pm::loadDefCol('FAnnuncio', array('idAnnuncio'));
+
+            while($check!=1) {
+                $new_num = rand(0, $numAnnunci - 1);
+                if (!in_array($new_num, $ran_num)) {
+                    $ran_num[] = $new_num;
+                    $check = 1;
+                }
+
+            }
             $annnunci_home[] = $pm::load('FAnnuncio', array(['idAnnuncio','=', $annunci_id[$ran_num[$i]]['idAnnuncio']]));
             $venditore_annuncio[] = $pm::load('FUtente', array(['idUser','=',$annnunci_home[$i]->getIdVenditore()]));
             $annunci_foto[] = $pm::load('FFotoAnnuncio', array(['idFoto','=',$annnunci_home[$i]->getIdFoto()]));
