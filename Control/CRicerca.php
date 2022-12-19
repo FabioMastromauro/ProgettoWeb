b8fdcb69d950a70d7e1d7e3e11695024200a223c
FabioMastromauro <mastromaurofabio@gmail.com>
2022-12-19 15:32:48 +0100

    Nella home si vedono solo gli annunci non acquistati

--- Control/CRicerca.php
+++ Control/CRicerca.php
@@ -17,8 +17,9 @@ class CRicerca
 
         $pm = USingleton::getInstance('FPersistentManager');
 
+        $annunci_id =$pm::load('FAnnuncio',array(['acquistato','=','0']));
 
-        $numAnnunci = $pm::getRows('FAnnuncio');
+        $numAnnunci = count($annunci_id =$pm::load('FAnnuncio',array(['acquistato','=','0'])));
         if($numAnnunci>6) $numAnnunci=6;
         $ran_num = array();
         $check = 0;
@@ -32,9 +33,9 @@ if($numAnnunci!=0) {
             }
         }
 
-        $annunci_id = $pm::loadDefCol('FAnnuncio', array('idAnnuncio'));
+        //$annunci_id = $pm::loadDefCol('FAnnuncio', array('idAnnuncio'));
         if ($numAnnunci == 1) $annunci_home[] = $pm::load('FAnnuncio', array(['idAnnuncio', '=', $annunci_id['idAnnuncio']]));
-        else $annunci_home[] = $pm::load('FAnnuncio',array(['idAnnuncio', '=', $annunci_id[$ran_num[$i]]['idAnnuncio']]));
+        else $annunci_home[] = $pm::load('FAnnuncio',array(['idAnnuncio', '=', $annunci_id[$ran_num[$i]]->getIdAnnuncio()]));
         $venditore_annuncio[] = $pm::load('FUtente', array(['idUser', '=', $annunci_home[$i]->getIdVenditore()]));
         $annunci_foto[] = $pm::load('FFotoAnnuncio', array(['idAnnuncio', '=', $annunci_home[$i]->getIdAnnuncio()]));
         if (!is_array($annunci_foto[$i])) $annunci_foto[$i] = array($annunci_foto[$i]);
